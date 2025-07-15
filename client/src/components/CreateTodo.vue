3cbf545dd84ca8b4f5bea6da8b45ad35bc5f0f11
Gyehyun Park <gyehyunbak@gmail.com>
2025-07-15 21:53:49 +0900

    refactor: Refactored some code

--- client/src/components/CreateTodo.vue
+++ client/src/components/CreateTodo.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useMutation, useQueryClient } from '@tanstack/vue-query'
-import { createTodo } from '@/api/createTodo' // 위에서 작성한 API
-import type { CreateTodoRequest } from '@/types/CreateTodoRequest'
+import { createTodo } from '@/api/createTodo'
 
 const title = ref('')
 const queryClient = useQueryClient()
 
 const { mutate, isPending } = useMutation({
-  mutationFn: (data: CreateTodoRequest) => createTodo(data),
+  mutationFn: createTodo,
   onSuccess: () => {
     title.value = ''
-    queryClient.invalidateQueries({ queryKey: ['todos'] }) // todos 다시 불러오기
+    queryClient.invalidateQueries({ queryKey: ['todos'] })
+  },
+  onError: (error) => {
+    console.error(error)
   },
 })
+
+const onClick = async () => {
+  mutate({ title: title.value })
+}
 </script>
 
 <template>
   <div>
     <input v-model="title" placeholder="할 일을 입력하세요" />
-    <button @click="mutate({ title })" :disabled="isPending || !title">추가</button>
+    <button @click="onClick" :disabled="isPending || !title">추가</button>
   </div>
 </template>
