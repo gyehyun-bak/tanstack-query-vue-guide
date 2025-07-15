d4fb18938ad771e4e0c5e3086776a2faeb3693e4
Gyehyun Park <gyehyunbak@gmail.com>
2025-07-15 16:35:37 +0900

    feature: useQuery

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+import { getTodos } from './api/getTodos'
+
+const { data, isPending, isError, error } = useQuery({
+  queryKey: ['todos'],
+  queryFn: getTodos,
+})
+</script>
 
 <template>
-  <main></main>
+  <span v-if="isPending">Loading...</span>
+  <span v-else-if="isError">Error: {{ error?.message }}</span>
+  <ul v-else-if="data">
+    <li v-for="todo in data" :key="todo.id">{{ todo.title }}</li>
+  </ul>
 </template>
